b42ebe6b954219fcfefb9d6e119fdfd9408ab1ad
Ákos Szarvák <metalbogyo@gmail.com>
2021-10-12 19:06:41 +0200

    initial commit

---
+++ src/components/SocialIcons.js
@@ -0,0 +1,30 @@
+import {
+  FaTwitter,
+  FaFacebook,
+  FaLinkedin,
+  FaInstagram,
+  FaGithub,
+} from "react-icons/fa";
+import { StyledSocialIcons } from "./styles/SocialIcons.styled";
+
+export default function SocialIcons() {
+  return (
+    <StyledSocialIcons>
+      <li>
+        <a href="https://instagram.com">
+          <FaInstagram />
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com">
+          <FaGithub />
+        </a>
+      </li>
+      <li>
+        <a href="https://linkedin.com">
+          <FaLinkedin />
+        </a>
+      </li>
+    </StyledSocialIcons>
+  );
+}
